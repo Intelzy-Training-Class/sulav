a25ed2797bbcea876c58aa280a009cca08c3c3f4
sulav subedi <sulavbdhsubedi12@gmail.com>
2025-05-23 13:31:44 +0545

    js

---
+++ index.js
@@ -0,0 +1,40 @@
+// // Age checker
+// let age = prompt("Enter your age");
+// if (age <= 18) {
+//   alert("You are a child");
+// } else if (age <= 50) {
+//   alert("You are an adult");
+// } else {
+//   alert("You are an old man");
+// }
+
+// // Even or odd
+// let num = prompt("Enter a number");
+// if (num % 2 === 0) {
+//   console.log("Even number");
+// } else {
+//   alert("odd number");
+// }
+
+// // grading system
+// let score = prompt("Enter your score");
+// if (score >= 90) {
+//   console.log("A grade");
+// } else if (score >= 80) {
+//   console.log("B grade");
+// } else if (score >= 70) {
+//   console.log("C grade");
+// } else if (score >= 60) {
+//   console.log("D grade");
+// } else {
+//   console.log("F");
+// }
+
+//login systemn
+let username = prompt("Enter your username");
+let password = prompt("Enter your password");
+if (username === "admin" && password === "1234") {
+  alert("Login successfull");
+} else {
+  alert("Login failed");
+}
